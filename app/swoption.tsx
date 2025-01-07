a91e5731c9a513216215c66f5ba5e9c9b306fdbc
sw-motors <seowonseat@naver.com>
2025-01-08 00:54:47 +0900

    17

--- app/swoption.tsx
+++ app/swoption.tsx
@@ -6,21 +6,21 @@ export const LoopSelect = [
 
 export const PackageSelect = {
     SW9_Low: [
-        { value: 'Family', name: '패밀리카 패키지', price: 5500000, image: '/public/image/family.png' },
-        { value: 'Daily', name: '데일리카 패키지', price: 4200000, image: '/image/daily.png' },
-        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000 , image: '/image/limousineMerge.png' }
+        { value: 'Family', name: '패밀리카 패키지', price: 5500000, image: 'https://drive.google.com/file/d/1Gq_xuPUhaGiXJswjwKOIekLa8iGpDdCV/view?usp=sharing' },
+        { value: 'Daily', name: '데일리카 패키지', price: 4200000, image: 'https://drive.google.com/file/d/1L2oztWsYulmmGMTEsoMXb-RJOxbMlmhE/view?usp=drive_link' },
+        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000 , image: 'https://drive.google.com/file/d/18eD7kxH799EJhGhVKgU_wLNw6t2I3Edu/view?usp=drive_link' }
     ],
     StandardEdge: [
-        { value: 'Premium6', name: '프리미엄 차박 패키지 (6인승)', price: 13000000, image: '/image/premium6.png' },
-        { value: 'Semi', name: '세미 의전 차박 패키지 (6인승)', price: 16000000, image: '/image/semi.png' },
-        { value: 'LimousineColor', name: '리무진 컬러 패키지 (9인승)', price: 6000000, image: '/image/limousineColor.png' },
-        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000, image: '/image/limousineMerge.png' }
+        { value: 'Premium6', name: '프리미엄 차박 패키지 (6인승)', price: 13000000, image: 'https://drive.google.com/file/d/1MzWGRDmkLChCVlkid-mJjJ4c5Dd8isCv/view?usp=drive_link' },
+        { value: 'Semi', name: '세미 의전 차박 패키지 (6인승)', price: 16000000, image: 'https://drive.google.com/file/d/1DuzACJLZAd6NC0EJJpp25BHejTnwgWbF/view?usp=drive_link' },
+        { value: 'LimousineColor', name: '리무진 컬러 패키지 (9인승)', price: 6000000, image: 'https://drive.google.com/file/d/1xvQtLir1yWZ3O4nms56x-dhJLmkWaW6A/view?usp=drive_link' },
+        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000, image: 'https://drive.google.com/file/d/18eD7kxH799EJhGhVKgU_wLNw6t2I3Edu/view?usp=drive_link' }
     ],
     SignatureMolding: [
-        { value: 'TheH', name: '더에이치 패키지', price: 13780000, image: '/image/theh.png' },
-        { value: 'Premium6', name: '프리미엄 차박 패키지 (6인승)', price: 13000000, image: '/image/premium6.png' },
-        { value: 'Semi', name: '세미 의전 차박 패키지 (6인승)', price: 16000000, image: '/image/semi.png' },
-        { value: 'LimousineColor', name: '리무진 컬러 패키지 (9인승)', price: 6000000, image: '/image/limousineColor.png' },
-        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000, image: '/image/limousineMerge.png' }
+        { value: 'TheH', name: '더에이치 패키지', price: 13780000, image: 'https://drive.google.com/file/d/1SLLtnAKdeWV7iUldFgJCqReDQZNIa7cE/view?usp=drive_link' },
+        { value: 'Premium6', name: '프리미엄 차박 패키지 (6인승)', price: 13000000, image: 'https://drive.google.com/file/d/1MzWGRDmkLChCVlkid-mJjJ4c5Dd8isCv/view?usp=drive_link' },
+        { value: 'Semi', name: '세미 의전 차박 패키지 (6인승)', price: 16000000, image: 'https://drive.google.com/file/d/1DuzACJLZAd6NC0EJJpp25BHejTnwgWbF/view?usp=drive_link' },
+        { value: 'LimousineColor', name: '리무진 컬러 패키지 (9인승)', price: 6000000, image: 'https://drive.google.com/file/d/1xvQtLir1yWZ3O4nms56x-dhJLmkWaW6A/view?usp=drive_link' },
+        { value: 'LimousineMerge', name: '리무진 통합 패키지 (9인승)', price: 6400000, image: 'https://drive.google.com/file/d/18eD7kxH799EJhGhVKgU_wLNw6t2I3Edu/view?usp=drive_link' }
     ]
 };
